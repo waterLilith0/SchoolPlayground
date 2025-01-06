36f69f8915ba0eaaae8958ccca2037389d762a51
Miriam Fix <fixm@landaumedia.de>
2025-01-06 10:45:09 +0100

    added einmaleins 5.2.2

---
+++ FahrkartenAutomat/A522_EinMalEins.java
@@ -0,0 +1,15 @@
+public class A522_EinMalEins {
+  public static void main(String[] args) {
+    // variables
+    // input
+    // processing
+    for (int i = 1; i <= 10; i++) {
+      for (int j = 1; j <= 10; j++) {
+        System.out.print((i * j) + " ");
+      }
+      System.out.println();
+    }
+
+    // output
+  }
+}
