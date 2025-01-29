56813e317fd6a2db72e723486a410fc84a420c6c
Miriam Fix <fixm@landaumedia.de>
2025-01-29 08:56:12 +0100

    finished A622_Passwort

---
+++ FahrkartenAutomat/A622_Passwort.java
@@ -0,0 +1,21 @@
+// Miriam Fix
+
+public class A622_Passwort {
+  public static void main(String[] args) {
+    for (int i = 0; i < 1000; i++) {
+      if (checkPasscode(i))
+        System.out.println(i);
+    }
+  }
+
+  public static boolean checkPasscode(int H4X0R) {
+    // gibt true zurück, falls das Passwort korrekt ist.
+    boolean result = false; 
+    for (int E1337 = 42; E1337 <= (52 ^ (0x6c)); E1337 += (3 << (14 % 6))) { 
+        if (result = ((++E1337 | E1337 + (2 >>> 1)) ^ (1 + H4X0R)) == (123456789 & 0)){
+            break; 
+        }
+    } 
+    return result;
+}
+}
