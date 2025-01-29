688ce551c9e3d141d2d74cf6b62d7b521293b1c9
Miriam Fix <fixm@landaumedia.de>
2025-01-29 08:55:47 +0100

    finished A621_Volumen

---
+++ FahrkartenAutomat/A621_Volumen.java
@@ -0,0 +1,11 @@
+// Miriam Fix
+
+public class A621_Volumen {
+  public static void main(String[] args) {
+    volumenWuerfel(1, 2, 3);
+  }
+
+  public static int volumenWuerfel(int a, int b, int h) {
+    return a * b * h;
+  }
+}
