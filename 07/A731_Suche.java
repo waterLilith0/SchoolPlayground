2786b41e12b02c2bf69b44d71b3a77dc51152fe8
Miriam Fix <fixm@landaumedia.de>
2025-02-27 13:30:48 +0100

    done with A731

--- 07/A731_Suche.java
+++ 07/A731_Suche.java
@@ -1,3 +1,29 @@
+// Miriam Fix
+// A731
+
+import java.util.Scanner;
+
 public class A731_Suche {
-  
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int searchNum = input.nextInt();
+
+    int[] zahlenArray = { 1, 5, 5, 7, 3, 11, 22, 23, 27 };
+    suche(zahlenArray, searchNum);
+       
+  }
+
+  static void suche(int[] zahlenArray, int wert) {
+    boolean found = false;
+    for (int i = 0; i < zahlenArray.length; i++) {
+      if(zahlenArray[i] == wert) {
+        found = true;
+      }
+    } 
+    if (found) {
+      System.out.println("Gefunden");
+    } else {
+      System.out.println("Nicht Gefunden");
+    }
+  }
 }
