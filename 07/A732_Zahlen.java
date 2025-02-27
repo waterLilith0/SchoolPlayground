ea3b9dcd357242591401d4441b52b46aa0de391a
Miriam Fix <fixm@landaumedia.de>
2025-02-27 14:42:30 +0100

    started A732

---
+++ 07/A732_Zahlen.java
@@ -0,0 +1,20 @@
+// Miriam Fix
+// A732
+
+public class A732_Zahlen {
+  public static void main(String[] args) {
+    int[] zahlen = {3,4,2,3,5,1,2,7,2,1,2,4};
+    most(zahlen);
+
+  }
+
+  public static void most(int[] array) {
+    
+  }
+
+  public static int[][] addMapElement(int map[][]) {
+    int newMap[][] = new [map.length + 1][];
+
+    return 0;
+  }
+}
